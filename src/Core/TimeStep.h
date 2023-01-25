627fc7a04e5216f8e92f9e07f015f70ba32f5406
Daniel Sloan <dsloan16@qub.ac.uk>
2023-01-25 22:36:12 +0000

    Updated timestep

--- src/Core/TimeStep.h
+++ src/Core/TimeStep.h
@@ -3,13 +3,20 @@
 
 class TimeStep {
 public:
-	TimeStep(float time = 0.0f)
-		: time(time) {};
+	TimeStep(float lastFrameTime, float time = 0.0f)
+		: time(time), lastframeTime(lastFrameTime) {};
 
 	operator float() const { return time; }
 
 	float GetSeconds() const { return time; }
 	float GetMilliseconds() const { return time * 1000.f; }
+
+	inline void Update(double currentTime)
+	{
+		time = currentTime - lastframeTime;
+		lastframeTime = currentTime;
+	};
 private:
 	float time;
+	float lastframeTime;
 };
