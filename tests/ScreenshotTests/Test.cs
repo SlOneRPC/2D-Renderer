b0b5290aabfed42fe49a5537b61c7684c65b36bb
SlOneRPC <dannyboy220900@gmail.com>
2023-06-26 20:52:32 +0100

    Implemented screenshot based testing

---
+++ tests/ScreenshotTests/Test.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenshotTests
+{
+    public class Test
+    {
+        public static bool allPassed = true;
+
+        public static void AssertTrue(bool result)
+        {
+            if (result)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Images match");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Images do not match!");
+                allPassed = false;
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void CheckIfAllPassed()
+        {
+            if (allPassed)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\n===========================");
+                Console.WriteLine("All tests passed!");
+                Console.WriteLine("===========================");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\n===========================");
+                Console.WriteLine("Not all tests passed!");
+                Console.WriteLine("===========================");
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
